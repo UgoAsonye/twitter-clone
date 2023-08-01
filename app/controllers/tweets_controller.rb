d6d7ec53ec36631ed412db7c3179b3573ed878f7
Ugo Asonye <ugonnaasonye@yahoo.com>
2023-07-31 19:55:14 -0500

    Adds in create function for tweeting

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,14 @@
 class TweetsController < ApplicationController
+  def create
+    @tweet = Tweet.new(
+      user_id: params[:user_id],
+      text: params[:text],
+      image: params[:image],
+    )
+    if @tweet.save
+      render json: { message: "Tweet has been tweeted!" }, status: :created
+    else
+      render json: { errors: @tweet.errors.full_messages }, status: :bad_request
+    end
+  end
 end
